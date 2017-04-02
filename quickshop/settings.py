3bde0aad95c9fd448d07231d2ec5ed7f739f9f00
dja <dja@example.com>
2017-04-02 17:08:41 +0000

    first views setup

--- quickshop/settings.py
+++ quickshop/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'qsh.apps.QshConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

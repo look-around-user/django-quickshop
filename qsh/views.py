4db59829f6ea78a67d1f847a7fe8e78e935e94e4
dja <dja@example.com>
2017-04-04 00:13:24 +0000

    Code to add many and delete from shoplist

--- qsh/views.py
+++ qsh/views.py
@@ -9,8 +9,12 @@ from .forms import BuydetailForm
 def index(request):
     return HttpResponse("halo")
 
-def shoplist_add_many(request, shoplist_id):
-    return HttpResponse("shoplist_add_many %s" % shoplist_id)
+def _shoplist_add_buydetail(shoplist, buyable, quantity):
+    buydetail = Buydetail()
+    buydetail.shoplist = shoplist
+    buydetail.buyable = buyable
+    buydetail.quantity = quantity
+    buydetail.save()
 
 def shoplist_add_new(request, shoplist_id):
 
@@ -19,29 +23,72 @@ def shoplist_add_new(request, shoplist_id):
     if request.method == 'POST':
         form = BuydetailForm(request.POST)
         if form.is_valid():
-            buyable = Buyable()
-            buyable.name = form.cleaned_data['name']
-            buyable.user = shoplist.user
-            buyable.save()
 
-            buydetail = Buydetail()
-            buydetail.shoplist = shoplist
-            buydetail.buyable = buyable
-            buydetail.quantity = form.cleaned_data['quantity']
-            buydetail.save()
+            buyable_name = form.cleaned_data['name']
 
-            return HttpResponseRedirect(reverse('qsh:shoplist_edit', args=(shoplist_id,)))
-    else:
-        form = BuydetailForm()
+            # don't create another if name already exists
+            buyable_lookup = Buyable.objects.filter(name=buyable_name, user=shoplist.user)
+            if buyable_lookup:
+                buyable = buyable_lookup
+            else:
+                buyable = Buyable()
+                buyable.name = buyable_name
+                buyable.user = shoplist.user
+                buyable.save()
+
+            _shoplist_add_buydetail(shoplist, buyable, form.cleaned_data['quantity'])
+
+            return HttpResponseRedirect(reverse('qsh:shoplist_edit', args=(shoplist.id,)))
+
+    form = BuydetailForm()
 
     return render(request, 'qsh/shoplist_add_new.html', { 'shoplist': shoplist, 'form': form })
+
+def shoplist_add_many(request, shoplist_id):
+
+    shoplist = Shoplist.objects.get(pk=shoplist_id)
+
+    if request.method == 'POST':
+        buyables_to_add = request.POST.getlist('buyables_to_add')
+
+        for buyable_id in buyables_to_add:
+            buyable = Buyable.objects.get(pk=buyable_id)
+            _shoplist_add_buydetail(shoplist, buyable, 1)
+
+        return HttpResponseRedirect(reverse('qsh:shoplist_edit', args=(shoplist.id,)))
+
+    buyables_all = Buyable.objects.all()
+    buydetails_in_shoplist = shoplist.buydetail_set.all()
+    buyables = []
+    for buyable in buyables_all:
+        is_in_shoplist = False
+        for buydetail in buydetails_in_shoplist:
+            if buydetail.buyable.id == buyable.id:
+                is_in_shoplist = True
+        if not is_in_shoplist:
+            buyables.append(buyable)
+
+    return render(request,
+        'qsh/shoplist_add_many.html', {
+            'shoplist': shoplist,
+            'buyables': buyables,
+            })
+
+def shoplist_edit(request, shoplist_id):
+
+    shoplist = Shoplist.objects.get(pk=shoplist_id)
+
+    if request.method == 'POST':
+        buydetails_to_delete = request.POST.getlist('buydetails_to_delete')
+
+        for buydetail_id in buydetails_to_delete:
+            buydetail = Buydetail.objects.get(pk=buydetail_id)
+            buydetail.delete()
+
+    return render(request, 'qsh/shoplist_edit.html', { 'shoplist': shoplist })
 
 class ShoplistsView(generic.ListView):
     model = Shoplist
     def get_queryset(self):
         # TODO filter by user
         return Shoplist.objects.all()
-
-class ShoplistEdit(generic.DetailView):
-    model = Shoplist
-    template_name = 'qsh/shoplist_edit.html'

a6daf30aaf8be40beceb0812b7740f50c45f5a61
dja <dja@example.com>
2017-04-09 22:29:56 +0000

    Moved business logic to models

--- qsh/models.py
+++ qsh/models.py
@@ -7,12 +7,63 @@ class Shoplist(models.Model):
     def __str__(self):
         return self.name
 
+    @staticmethod
+    def get_one(pk, user):
+        for shoplist in Shoplist.objects.filter(pk=pk, user=user):
+            return shoplist
+        return None
+
+    @staticmethod
+    def save_new(name, user):
+        shoplist = Shoplist()
+        shoplist.name = name
+        shoplist.user = user
+        shoplist.save()
+        return shoplist
+
+    def add_buydetail(self, buyable, quantity):
+        buydetail = Buydetail()
+        buydetail.shoplist = self
+        buydetail.buyable = buyable
+        buydetail.quantity = quantity
+        buydetail.save()
+
+    def get_unused_buyables(self):
+        buyables_all = Buyable.objects.filter(user=self.user)
+        buydetails_in_shoplist = self.buydetail_set.all()
+        buyables = []
+        for buyable in buyables_all:
+            is_in_shoplist = False
+            for buydetail in buydetails_in_shoplist:
+                if buydetail.buyable.id == buyable.id:
+                    is_in_shoplist = True
+            if not is_in_shoplist:
+                buyables.append(buyable)
+        return buyables
+
 class Buyable(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     #last_use = models.DateField()
     def __str__(self):
         return self.name
+
+    @staticmethod
+    def save_new(name, user):
+        buyable = Buyable()
+        buyable.name = name
+        buyable.user = user
+        buyable.save()
+        return buyable
+
+    @staticmethod
+    def fetch_or_create(name, user):
+        buyable_lookup = Buyable.objects.filter(name=name, user=user)
+        if buyable_lookup:
+            buyable = buyable_lookup
+        else:
+            buyable = Buyable.save_new(name, user)
+        return buyable
 
 class Buydetail(models.Model):
     shoplist = models.ForeignKey(Shoplist, on_delete=models.CASCADE)
--- qsh/views.py
+++ qsh/views.py
@@ -9,67 +9,29 @@ from .models import Shoplist, Buyable, Buydetail, User
 from .forms import BuydetailForm, ShoplistForm
 
 @login_required
-def _shoplist_add_buydetail(shoplist, buyable, quantity):
-    buydetail = Buydetail()
-    buydetail.shoplist = shoplist
-    buydetail.buyable = buyable
-    buydetail.quantity = quantity
-    buydetail.save()
-
-@login_required
 def shoplist_add_new(request, shoplist_id):
-
-    shoplist = Shoplist.objects.get(pk=shoplist_id)
-
+    shoplist = Shoplist.get_one(shoplist_id, request.user)
     if request.method == 'POST':
         form = BuydetailForm(request.POST)
         if form.is_valid():
-
             buyable_name = form.cleaned_data['name']
-
-            # don't create another if name already exists
-            buyable_lookup = Buyable.objects.filter(name=buyable_name, user=shoplist.user)
-            if buyable_lookup:
-                buyable = buyable_lookup
-            else:
-                buyable = Buyable()
-                buyable.name = buyable_name
-                buyable.user = request.user
-                buyable.save()
-
-            _shoplist_add_buydetail(shoplist, buyable, form.cleaned_data['quantity'])
-
+            buyable = Buyable.fetch_or_create(name=buyable_name, user=request.user)
+            shoplist.add_buydetail(buyable, form.cleaned_data['quantity'])
             return HttpResponseRedirect(reverse('qsh:shoplist_edit', args=(shoplist.id,)))
-
     form = BuydetailForm()
-
     return render(request, 'qsh/shoplist_add_new.html', { 'shoplist': shoplist, 'form': form })
 
 @login_required
 def shoplist_add_many(request, shoplist_id):
-
-    shoplist = Shoplist.objects.get(pk=shoplist_id)
-
+    shoplist = Shoplist.get_one(shoplist_id, request.user)
     if request.method == 'POST':
         buyables_to_add = request.POST.getlist('buyables_to_add')
-
         for buyable_id in buyables_to_add:
             buyable = Buyable.objects.get(pk=buyable_id)
-            _shoplist_add_buydetail(shoplist, buyable, 1)
-
+            shoplist.add_buydetail(buyable, 1)
         return HttpResponseRedirect(reverse('qsh:shoplist_edit', args=(shoplist.id,)))
 
-    buyables_all = Buyable.objects.filter(user=request.user)
-    buydetails_in_shoplist = shoplist.buydetail_set.all()
-    buyables = []
-    for buyable in buyables_all:
-        is_in_shoplist = False
-        for buydetail in buydetails_in_shoplist:
-            if buydetail.buyable.id == buyable.id:
-                is_in_shoplist = True
-        if not is_in_shoplist:
-            buyables.append(buyable)
-
+    buyables = shoplist.get_unused_buyables()
     return render(request,
         'qsh/shoplist_add_many.html', {
             'shoplist': shoplist,
@@ -78,16 +40,12 @@ def shoplist_add_many(request, shoplist_id):
 
 @login_required
 def shoplist_edit(request, shoplist_id):
-
-    shoplist = Shoplist.objects.get(pk=shoplist_id)
-
+    shoplist = Shoplist.get_one(shoplist_id, request.user)
     if request.method == 'POST':
         buydetails_to_delete = request.POST.getlist('buydetails_to_delete')
-
         for buydetail_id in buydetails_to_delete:
-            buydetail = Buydetail.objects.get(pk=buydetail_id)
+            buydetail = Buydetail.objects.filter(pk=buydetail_id, shoplist=shoplist)
             buydetail.delete()
-
     return render(request, 'qsh/shoplist_edit.html', { 'shoplist': shoplist })
 
 @login_required
@@ -95,15 +53,9 @@ def shoplist_create(request):
     if request.method == 'POST':
         form = ShoplistForm(request.POST)
         if form.is_valid():
-
             shoplist_name = form.cleaned_data['name']
             # TODO error message for same name
-
-            shoplist = Shoplist()
-            shoplist.name = shoplist_name
-            shoplist.user = request.user
-            shoplist.save()
-
+            Shoplist.save_new(shoplist_name, request.user)
             return HttpResponseRedirect(reverse('qsh:shoplist_edit', args=(shoplist.id,)))
     form = ShoplistForm()
 
@@ -111,12 +63,11 @@ def shoplist_create(request):
 
 @login_required
 def shoplist_delete(request):
-
     if request.method == 'POST':
         shoplists_to_delete = request.POST.getlist('shoplists_to_delete')
 
         for shoplist_id in shoplists_to_delete:
-            shoplist = Shoplist.objects.filter(pk=shoplist_id)
+            shoplist = Shoplist.get_one(shoplist_id, request.user)
             for buydetail in Buydetail.objects.filter(shoplist=shoplist):
                 buydetail.delete()
             shoplist.delete()
